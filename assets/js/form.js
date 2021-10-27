9f937cf9cc718e5da64d6bcb9fcbd593f247f2ea
ILMatsa <nachomazza3@gmail.com>
2021-10-26 23:18:39 -0300

    mejoras ultimas

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,3 +1,7 @@
+var cambioColor = document.getElementById("inputBox")
+
+cambioColor.className = "inputBox"
+
 function validar(){
     var nombre, email, usuarios, expresiones;
     nombre = document.getElementById("nombre").value;       //El valor que esta en ese campo se va a aguardar en la variable nombre
@@ -9,23 +13,27 @@ function validar(){
 
 
     if (nombre === "" ||email === "" ||usuarios === ""){
+        cambioColor.className = "inputBox-incorrecto"
         alert("POR FAVOR, COMPLETE LOS CAMPOS OBLIGATORIOS");
-        
         return false;        
     }
     else if (nombre.lengyh > 35){  //Si la cantidad de caracteres es menos a 35...
+        cambioColor.className = "inputBox-incorrecto"
         alert("NOMBRE DEMASIADO LARGO");
         return false;     
     }
     else if (!expresiones.test(email)){  
+        cambioColor.className = "inputBox-incorrecto"
         alert("EL CORREO DEBE ESTAR COMPLETO");
         return false;     
     }
     else if (usuarios.lengyh > 1 || isNaN(usuarios)){
+        cambioColor.className = "inputBox-incorrecto"
         alert("COMO MINIMO DEBE TENER 1 USUARIO");
         return false;
     }
     else {
+        cambioColor.className = "inputBox"
         return true;
     }
 }
