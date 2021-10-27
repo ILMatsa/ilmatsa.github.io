c1ad647b70ae747fcfa12217df58d7de739a873a
ILMatsa <nachomazza3@gmail.com>
2021-10-27 12:39:32 -0300

    Arreglos de errores del form

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,7 +1,3 @@
-var cambioColor = document.getElementById("inputBox")
-
-cambioColor.className = "inputBox"
-
 function validar(){
     var nombre, email, usuarios, expresiones;
     nombre = document.getElementById("nombre").value;       //El valor que esta en ese campo se va a aguardar en la variable nombre
@@ -9,31 +5,28 @@ function validar(){
     empresa = document.getElementById("empresa").value;
     soporte = document.getElementById("soporte").value;
     usuarios = document.getElementById("usuarios").value;
-    expresiones = /\w+@+\w+\.+[a-z]/;       //https://developer.mozilla.org/es/docs/Web/JavaScript/Guide/Regular_Expressions/Cheatsheet
+    expresionesEmail = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+    expresionesNombre = /^[a-zA-ZÀ-ÿ\s]{1,40}$/;       //https://developer.mozilla.org/es/docs/Web/JavaScript/Guide/Regular_Expressions/Cheatsheet
 
 
     if (nombre === "" ||email === "" ||usuarios === ""){
-        cambioColor.className = "inputBox-incorrecto"
         alert("POR FAVOR, COMPLETE LOS CAMPOS OBLIGATORIOS");
         return false;        
     }
-    else if (nombre.lengyh > 35){  //Si la cantidad de caracteres es menos a 35...
-        cambioColor.className = "inputBox-incorrecto"
-        alert("NOMBRE DEMASIADO LARGO");
+    else if (!expresionesNombre.test(nombre)){  //Si la cantidad de caracteres es menos a 35...
+        alert("NOMBRE INCORRECTO O MUY LARGO");
         return false;     
     }
-    else if (!expresiones.test(email)){  
-        cambioColor.className = "inputBox-incorrecto"
+    else if (!expresionesEmail.test(email)){  
         alert("EL CORREO DEBE ESTAR COMPLETO");
         return false;     
     }
-    else if (usuarios.lengyh > 1 || isNaN(usuarios)){
-        cambioColor.className = "inputBox-incorrecto"
+    else if (usuarios.lengyh < 1 || isNaN(usuarios)){
         alert("COMO MINIMO DEBE TENER 1 USUARIO");
         return false;
     }
     else {
-        cambioColor.className = "inputBox"
+
         return true;
     }
 }
